56b71e7b89fc891996ca5cae1028a5bcbbdad3bb
Narayan.P <narayan.parvatikar@nvisionx.ai>
2025-06-25 18:40:30 +0530

    networking module - adding vpc endpoint

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,3 @@ output "public_subnet_ids" {
   value = var.create_networking_resources ? [for s in aws_subnet.public : s.id] : []
 }
 
-output "private_subnet_ids" {
-  value = var.create_networking_resources ? [for s in aws_subnet.private : s.id] : var.private_subnet_ids
-}
-
